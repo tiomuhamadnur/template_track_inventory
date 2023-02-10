bc9b4b1f8686cb79f2e76ce11c23c637d7135c9f
Tio Muhamad Nur <outsider.kuningan52@gmail.com>
2023-02-11 01:06:42 +0700

    update

--- resources/views/transisi/transisi.blade.php
+++ resources/views/transisi/transisi.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width,intial-scale=1.0">
     <meta charset="utf-8">
     <title>Dashboard Switch</title>
-    <link rel="stylesheet" href="{{ asset('assets/transisiflex/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/transisiflex/style2.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 </head>
 
