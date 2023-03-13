0da8ce7ade6dd5eabb61759bca4674acaf96691f
Tio Muhamad Nur <outsider.kuningan52@gmail.com>
2023-03-13 10:49:44 +0700

    update

--- resources/views/depo/depo_temuan/export-pdf.blade.php
+++ resources/views/depo/depo_temuan/export-pdf.blade.php
@@ -2,29 +2,45 @@
 
 <head>
     <title>List Temuan Depo</title>
+    <link rel="shortcut icon" href="{{ asset('assets/images/mm.png') }}" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
 <body>
-    <h3 class="text-center fw-bolder">List Temuan Depo</h3>
+    <table border="1" style="width: 100%" class="mb-3">
+        <thead>
+            <th class="text-center">
+                <img style="height: 70px" src="{{ public_path('assets/images/tcsm.png') }}" alt="logo-marti">
+            </th>
+            <th class="text-center">
+                <h3 class="fw-bolder mx-auto mb-0">List Temuan Depo</h3>
+                <h6>
+                    (Update: {{ \Carbon\Carbon::now()->format('d M Y') }})
+                </h6>
+            </th>
+            <th class="text-center">
+                <img style="height: 70px" src="{{ public_path('assets/images/logo_mrtj.png') }}" alt="logo-mrt">
+            </th>
+        </thead>
+    </table>
     <table border="1" class="text-center">
         <thead>
             <tr>
-                <th>No</th>
-                <th>Date</th>
-                <th>Line</th>
-                <th>Chainage</th>
-                <th>Dir</th>
-                <th>Part</th>
-                <th>Detail Part</th>
-                <th>Defect</th>
-                <th>Remark</th>
-                <th>Class</th>
-                <th>Examiner</th>
-                <th>Status</th>
-                <th>Photo</th>
+                <th class="p-1">No</th>
+                <th class="p-1">Date</th>
+                <th class="p-1">Line</th>
+                <th class="p-1">Chainage (m)</th>
+                <th class="p-1">Dir</th>
+                <th class="p-1">Part</th>
+                {{-- <th class="p-1">Detail Part</th> --}}
+                <th class="p-1">Defect</th>
+                <th class="p-1">Remark</th>
+                <th class="p-1">Class</th>
+                <th class="p-1">Examiner</th>
+                <th class="p-1">Status</th>
+                <th class="p-1">Photo</th>
             </tr>
         </thead>
         <tbody>
@@ -46,13 +62,14 @@
                         {{ $item->direction }}
                     </td>
                     <td>
-                        {{ $item->part->name }}
+                        {{ $item->part->name }} <br>
+                        ({{ $item->detail_part->name }})
                     </td>
-                    <td>
+                    {{-- <td>
                         {{ $item->detail_part->name }}
-                    </td>
+                    </td> --}}
                     <td>
-                        {{ $item->defect->name ?? '-' }}
+                        {{ $item->defect->name ?? 'Lainnya' }}
                     </td>
                     <td>
                         {{ $item->remark ?? '-' }}
@@ -66,7 +83,7 @@
                     <td>
                         {{ $item->status }}
                     </td>
-                    <td>
+                    <td class="p-2">
                         <img style="height: 120px" src="{{ public_path('storage/' . $item->photo) }}"
                             alt="tidak ada foto">
                     </td>
--- resources/views/mainline/mainline_temuan/export-pdf.blade.php
+++ resources/views/mainline/mainline_temuan/export-pdf.blade.php
@@ -2,32 +2,48 @@
 
 <head>
     <title>List Temuan Mainline</title>
+    <link rel="shortcut icon" href="{{ asset('assets/images/mm.png') }}" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
 <body>
-    <h3 class="text-center fw-bolder">List Temuan Mainline</h3>
+    <table border="1" style="width: 100%" class="mb-3">
+        <thead>
+            <th class="text-center">
+                <img style="height: 70px" src="{{ public_path('assets/images/tcsm.png') }}" alt="logo-marti">
+            </th>
+            <th class="text-center">
+                <h3 class="fw-bolder mx-auto mb-0">List Temuan Mainline</h3>
+                <h6>
+                    (Update: {{ \Carbon\Carbon::now()->format('d M Y') }})
+                </h6>
+            </th>
+            <th class="text-center">
+                <img style="height: 70px" src="{{ public_path('assets/images/logo_mrtj.png') }}" alt="logo-mrt">
+            </th>
+        </thead>
+    </table>
     <table border="1" class="text-center">
         <thead>
             <tr>
-                <th>No</th>
-                <th>Date</th>
-                <th>Area</th>
-                <th>Line</th>
-                <th>Span</th>
-                <th>Chainage</th>
-                <th>No. <br> Sleeper</th>
-                <th>Dir</th>
-                <th>Part</th>
-                <th>Detail <br> Part</th>
-                <th>Defect</th>
-                <th>Remark</th>
-                <th>Class</th>
-                <th>Examiner</th>
-                <th>Status</th>
-                <th>Photo</th>
+                <th class="p-1">No</th>
+                <th class="p-1">Date</th>
+                <th class="p-1">Area</th>
+                <th class="p-1">Line</th>
+                <th class="p-1">Span</th>
+                {{-- <th class="p-1">Chainage</th> --}}
+                <th class="p-1">No. <br> Sleeper</th>
+                <th class="p-1">Dir</th>
+                <th class="p-1">Part</th>
+                {{-- <th class="p-1">Detail <br> Part</th> --}}
+                <th class="p-1">Defect</th>
+                <th class="p-1">Remark</th>
+                <th class="p-1">Class</th>
+                <th class="p-1">Examiner</th>
+                <th class="p-1">Status</th>
+                <th class="p-1">Photo</th>
             </tr>
         </thead>
         <tbody>
@@ -48,9 +64,9 @@
                     <td>
                         {{ $item->mainline->no_span }}
                     </td>
-                    <td>
+                    {{-- <td>
                         {{ $item->mainline->kilometer }}
-                    </td>
+                    </td> --}}
                     <td>
                         {{ $item->no_sleeper }}
                     </td>
@@ -58,13 +74,14 @@
                         {{ $item->direction }}
                     </td>
                     <td>
-                        {{ $item->part->name }}
+                        {{ $item->part->name }} <br>
+                        ({{ $item->detail_part->name }})
                     </td>
-                    <td>
+                    {{-- <td>
                         {{ $item->detail_part->name }}
-                    </td>
+                    </td> --}}
                     <td>
-                        {{ $item->defect->name ?? '-' }}
+                        {{ $item->defect->name ?? 'Lainnya' }}
                     </td>
                     <td>
                         {{ $item->remark ?? '-' }}
@@ -78,7 +95,7 @@
                     <td>
                         {{ $item->status }}
                     </td>
-                    <td>
+                    <td class="p-2">
                         <img style="height: 120px" src="{{ public_path('storage/' . $item->photo) }}"
                             alt="tidak ada foto">
                     </td>
