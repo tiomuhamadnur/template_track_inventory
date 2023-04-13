2b7b4ec2bbe24437bb55d7b1c4f9534ca582147e
deirfed <dedeirfan@Dedes-MacBook-Pro.local>
2023-04-13 10:22:01 +0700

    tiitk notif

--- resources/views/mainline/mainline_layout/sidebar.blade.php
+++ resources/views/mainline/mainline_layout/sidebar.blade.php
@@ -31,7 +31,7 @@
         </li>
         <li class="nav-item animate__animated animate__fadeInLeft">
             <a class="nav-link" href="{{ route('rfi.mainline.index') }}">
-                <i class="mdi mdi-airplane menu-icon"></i>
+                <i class="mdi mdi-arrange-send-backward menu-icon bell-notif"></i>
                 <span class="menu-title">Request For Inpection</span>
             </a>
         </li>
@@ -59,3 +59,32 @@
         </li>
     </ul>
 </nav>
+
+<style>
+    .bell-notif {
+    color: #fff;
+    position: relative;
+    transform-origin: top;
+
+  }
+  .fa-bell {
+    font-size: 5px;
+  }
+
+  .bell-notif::after {
+    content: attr(current-count);
+    position: absolute;
+    top: 0;
+    right: -10%;
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    /* background-color: red; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 4px;
+    border: 4px solid red;
+
+  }
+</style>
